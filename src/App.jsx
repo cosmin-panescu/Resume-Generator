309c7d3078dfbd4b70aa21447d0d12b51a0b01d0
Panescu Cosmin <1panescu.cosmin@gmail.com>
2023-03-26 09:41:41 +0300

    Add Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import useLocalStorage from './hooks/useLocalStorage'
+// React
+import React from 'react';
+// pages
 import AppPage from './pages/appPage/AppPage'
+import HomePage from './pages/homePage/HomePage'
+// React Router
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
 
   return (
     <div className='app'>
-      <AppPage />
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route path='/resume' element={<AppPage />} />
+      </Routes>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom';
+// CSS
 import './index.scss';
+// components
 import App from './App';
+// React Router 
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
