6485a86b7191faf0c09557eecae69d58749db548
Panescu Cosmin <1panescu.cosmin@gmail.com>
2023-03-31 21:55:00 +0300

    Add modal for zoomming issue and for small screens

--- src/pages/appPage/AppPage.jsx
+++ src/pages/appPage/AppPage.jsx
@@ -10,16 +10,21 @@ import './AppPage.scss'
 import { useReactToPrint } from 'react-to-print'
 // html & css data
 import { cssData, htmlData } from '../../utils/codeContent';
+// React Router
 import { Link } from 'react-router-dom'
-
+// icons
+import { CiWarning } from 'react-icons/ci';
 
 const AppPage = () => {
-  // with localStorage
+  // localStorage for template
   const [html, setHtml] = useLocalStorage('html', htmlData);
   const [css, setCss] = useLocalStorage('css', cssData);
-  // without localStorage
-  // const [html, setHtml] = useState(htmlData);
-  // const [css, setCss] = useState(cssData);
+
+  // state for modal
+  const [modalOpen, setModalOpen] = useState(true);
+
+  // track screen size
+  const smallViewport = window.innerWidth < 1024;
 
   // print as PDF
   const resumeRef = useRef();
@@ -28,7 +33,7 @@ const AppPage = () => {
     documentTitle: 'Resume',
   });
 
-  // all code-source document (HTML + CSS) 
+  // all source-code document (HTML + CSS) 
   const srcDoc = `
         <html>
           <body>${html}</body>
@@ -38,7 +43,39 @@ const AppPage = () => {
 
   return (
     <div className='app-page'>
-      <div className="panel left-panel">
+      {smallViewport ? (
+        <div className={`modal ${!modalOpen && "close"}`}>
+          <CiWarning className='modal-icon' />
+          <p className='modal-text'>
+            Small Screen Detected. Using betteresume on a small screen or mobile device is not very pleasant. I recomment you to use this resume generator on a computer for reviewing, creating & exporting resumes.
+          </p>
+          <button
+            className='modal-close custom-btn1'
+            onClick={() => setModalOpen(false)}
+          >
+            Got it
+            <div className="arrow-wrapper">
+              <div className="arrow"></div>
+            </div>
+          </button>
+        </div>
+      ) : (
+        <div className={`modal ${!modalOpen && "close"}`}>
+          <CiWarning className='modal-icon' />
+          <p className='modal-text'>Depending on the size of your screen, the preview of your resume may be distorted. However, when you download the resume, it will be normal, with the standard size for a resume. You can solve this issue by zooming out the page.
+          </p>
+          <button
+            className='modal-close custom-btn1'
+            onClick={() => setModalOpen(false)}
+          >
+            Got it
+            <div className="arrow-wrapper">
+              <div className="arrow"></div>
+            </div>
+          </button>
+        </div>
+      )}
+      <div className={`panel left-panel ${modalOpen && "overlay"}`}>
         <Editor
           language="xml"
           displayName="HTML"
@@ -53,7 +90,7 @@ const AppPage = () => {
         />
       </div>
 
-      <div className="panel right-panel">
+      <div className={`panel right-panel ${modalOpen && "overlay"}`}>
         <div className="resume-buttons">
           <Link
             className='custom-btn1 link link-reverse'
